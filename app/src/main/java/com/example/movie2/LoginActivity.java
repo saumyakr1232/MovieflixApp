738e881996d6ffa21159e7c658eb1d39c4115bde
saumyakr1232 <53995365+saumyakr1232@users.noreply.github.com>
2020-03-25 00:47:44 +0530

    24/03

--- app/src/main/java/com/example/movie2/LoginActivity.java
+++ app/src/main/java/com/example/movie2/LoginActivity.java
@@ -1,17 +1,60 @@
 package com.example.movie2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private Button btnLogin2;
+    private EditText edtTxtemail, editTxtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+        btnLogin2 = (Button) findViewById(R.id.btnSignIn2);
+
+        edtTxtemail = (EditText) findViewById(R.id.editTxtEmailLoginPage);
+        editTxtPass = (EditText) findViewById(R.id.editTextPassLoginPage);
+
+        btnLogin2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtTxtemail.getText().toString().isEmpty() || editTxtPass.getText().toString().isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Empty Fields are not allowed", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(edtTxtemail.getText().toString(), editTxtPass.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Logged in Succesfully", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Faild to login ", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
 
     }
+
 }
--- app/src/main/java/com/example/movie2/SignupActivity.java
+++ app/src/main/java/com/example/movie2/SignupActivity.java
@@ -1,14 +1,89 @@
 package com.example.movie2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
 public class SignupActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private Button btnSignUp2;
+    private EditText edtTxtFirstName, edtTxtLastName, edtTxtEmail, edtTxtPass, edtTxtConfPass;
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        widgets();
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSignUp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtTxtEmail.getText().toString().isEmpty() || edtTxtFirstName.getText().toString().isEmpty() || edtTxtLastName.getText().toString().isEmpty() || edtTxtPass.getText().toString().isEmpty() || edtTxtConfPass.getText().toString().isEmpty()) {
+                    Toast.makeText(SignupActivity.this, "Empty Fields are not allowed", Toast.LENGTH_LONG).show();
+                    return;
+                } else if (Objects.equals(edtTxtPass.getText().toString(), edtTxtConfPass.getText().toString())) {
+                    mAuth.createUserWithEmailAndPassword(edtTxtEmail.getText().toString(), edtTxtPass.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(SignupActivity.this, "User is registered", Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+
+
+                                    } else {
+                                        Toast.makeText(SignupActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                } else {
+                    Toast.makeText(SignupActivity.this, "Passwords must be same", Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    public void widgets() {
+        btnSignUp2 = (Button) findViewById(R.id.btnSignUp2);
+
+        edtTxtFirstName = (EditText) findViewById(R.id.edtTxtFirstName);
+        edtTxtLastName = (EditText) findViewById(R.id.edtTxtLastName);
+        edtTxtEmail = (EditText) findViewById(R.id.edtTxtEmailIdSignUp);
+        edtTxtPass = (EditText) findViewById(R.id.edtTxtPassword);
+        edtTxtConfPass = (EditText) findViewById(R.id.edtTxtConfPass);
+        textView = (TextView) findViewById(R.id.txtViewSignIn);
+    }
+
+
+
 }
