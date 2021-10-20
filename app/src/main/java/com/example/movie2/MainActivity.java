6543837252a3deb8049a778abb1290144cd8236b
saumya <saumyakr181999@gmail.com>
2021-10-20 21:36:24 +0530

    minor chnages

--- app/src/main/java/com/example/movie2/MainActivity.java
+++ app/src/main/java/com/example/movie2/MainActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private Utils utils;
+    private ImageView imageView;
 
 
     @Override
@@ -26,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         }
+
+        imageView = findViewById(R.id.landing_page_iv);
+        Glide.with(this).asBitmap().load("https://image.tmdb.org/t/p/w1280/lmZFxXgJE3vgrciwuDib0N8CfQo.jpg").centerCrop().into(imageView);
 
         findViewById(R.id.getStartedButton).setOnClickListener(new View.OnClickListener() {
             @Override
